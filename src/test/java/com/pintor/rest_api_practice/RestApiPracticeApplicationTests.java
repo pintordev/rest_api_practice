9714e37f5b882f5cd26f8d59fa0aab19b8f5bcff
pintor.dev <pintor.dev@gmail.com>
2023-11-24 23:45:41 +0900

    #1 - 프로젝트 세팅

---
+++ src/test/java/com/pintor/rest_api_practice/RestApiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pintor.rest_api_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
